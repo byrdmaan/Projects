0018c4c9c2ead79732223829f76ef5ad7c58cfa8
Daniel <dpeck@smu.edu>
2016-09-23 15:38:39 -0700

    more cook

--- Quench/cook/js/vibes.js
+++ Quench/cook/js/vibes.js
@@ -3,8 +3,12 @@ var vibe = document.getElementById("rec_song");
 function playVibe() {
     vibe.play();
     $("#rec_song").prop("volume", 0.25);
+    $('#play_button').prop('disabled', true);
+    $('#pause_button').prop('disabled', false);
 }
 
 function pauseVibe() {
     vibe.pause();
+    $('#play_button').prop('disabled', false);
+    $('#pause_button').prop('disabled', true);
 }
